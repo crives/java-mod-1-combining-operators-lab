7ffc33615eeef012bad0506cafbb3d3c6159e739
Caroline Rives <car.rives@gmail.com>
2022-06-23 12:51:09 -0400

    Operators Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    static boolean isTrue = true;
+    static boolean isFalse = false;
     public static void main(String[] args) {
-        // your code here
+        System.out.println("Truth table for && operator");
+        System.out.println("true && true = " + (isTrue && isTrue));
+        System.out.println("true && false = " + (isTrue && isFalse));
+        System.out.println("false && false = " + (isFalse && isFalse));
+        System.out.println("false && true = " + (isFalse && isTrue));
     }
 }
